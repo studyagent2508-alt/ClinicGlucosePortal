7a24a5245321798ef4252ec7a1ae4af2d59c3609
studyagent2508-alt <studyagent2508@gmail.com>
2026-09-22 17:11:42 +0530

    Add frontend AWS configuration

---
+++ frontend/config.js
@@ -0,0 +1,10 @@
+(() => {
+  "use strict";
+
+  // Public application identifiers only. Never place AWS credentials or secrets here.
+  window.KAARYA_AWS_CONFIG = Object.freeze({
+    region: "ap-south-1",
+    clientId: "4s7k205jmn9nhq8ghna92brmuf",
+    apiBaseUrl: "https://apzc0jbwl0.execute-api.ap-south-1.amazonaws.com",
+  });
+})();
